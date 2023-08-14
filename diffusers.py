0cb8537c6cb646f243df79a1ee974aac6da23549
sho-okumoto <115537918+sho-okumoto@users.noreply.github.com>
2023-08-15 02:31:58 +0900

    WIP

--- diffusers.py
+++ diffusers.py
@@ -1,6 +1,6 @@
 import torch
 
-class DDPMDiffuser():
+class DDPMDiffuser1d():
     """
         Denoising Diffusion Probabilistic Model (DDPM) diffuser.
         Reference: https://arxiv.org/pdf/2006.11239.pdf
@@ -12,11 +12,11 @@ class DDPMDiffuser():
             Beta values for each time step
     """
 
-    def __init__(self, betas, device='cuda:0'):
-        self.device = device
-        self.betas = betas.to(device)
+    def __init__(self, betas):
+        self.device = betas.device
+        self.betas = betas
         self.alphas = 1 - betas
-        self.alpha_bars = torch.cumprod(self.alphas, dim=0).to(device)
+        self.alpha_bars = torch.cumprod(self.alphas, dim=0).to(self.device)
 
     def diffuse(self, X_0, t):
         """
@@ -24,19 +24,66 @@ class DDPMDiffuser():
 
             Parameters
             ----------
-            t: int
+            t: torch.Tensor of shape [N]
                 Time step
-            X_0: torch.Tensor of shape [N, D]
+            X_0: torch.Tensor of shape [N, C, D]
                 Data to be diffused
 
             Returns
             -------
-            X_t: torch.Tensor of shape [N, D]
+            X_t: torch.Tensor of shape [N, C, D]
                 Diffused data
-            eps: torch.Tensor of shape [N, D]
+            eps: torch.Tensor of shape [N, C, D]
                 Noise added to data
         """
 
         eps = torch.randn(X_0.size(), dtype=torch.float32, device=self.device)
-        X_t = torch.sqrt(self.alpha_bars[t]) * X_0 + torch.sqrt(1 - self.alpha_bars[t]) * eps
+
+        mean_scale = torch.sqrt(self.alpha_bars[t])[:, None, None]
+        var_scale = torch.sqrt(1 - self.alpha_bars[t])[:, None, None]
+        X_t = mean_scale * X_0 + var_scale * eps
+        return X_t, eps
+
+class DDPMDiffuser2d():
+    """
+        Denoising Diffusion Probabilistic Model (DDPM) diffuser for 2D data.
+        Reference: https://arxiv.org/pdf/2006.11239.pdf
+
+        Parameters
+        ----------
+    
+        betas: torch.Tensor of shape [T]
+            Beta values for each time step
+    """
+
+    def __init__(self, betas):
+        self.device = betas.device
+        self.betas = betas
+        self.alphas = 1 - betas
+        self.alpha_bars = torch.cumprod(self.alphas, dim=0).to(self.device)
+
+    def diffuse(self, X_0, t):
+        """
+            Diffuse 2D data X_0 at time step t.
+
+            Parameters
+            ----------
+            t: torch.Tensor of shape [N]
+                Time step
+            X_0: torch.Tensor of shape [N, C, H, W]
+                2D Data to be diffused
+
+            Returns
+            -------
+            X_t: torch.Tensor of shape [N, C, H, W]
+                Diffused 2D data
+            eps: torch.Tensor of shape [N, C, H, W]
+                Noise added to 2D data
+        """
+
+        eps = torch.randn(X_0.size(), dtype=torch.float32, device=self.device)
+
+        mean_scale = torch.sqrt(self.alpha_bars[t])[:, None, None, None]
+        var_scale = torch.sqrt(1 - self.alpha_bars[t])[:, None, None, None]
+        X_t = mean_scale * X_0 + var_scale * eps
         return X_t, eps
--- samplers.py
+++ samplers.py
@@ -1,6 +1,6 @@
 import torch
 
-class DDPMSampler():
+class DDPMSampler1d():
     """
         Denoising Diffusion Probabilistic Model (DDPM) sampler.
         Reference: https://arxiv.org/pdf/2006.11239.pdf
@@ -11,12 +11,12 @@ class DDPMSampler():
             Beta values for each time step
     """
 
-    def __init__(self, betas: torch.Tensor, noise_model, device: str = 'cuda:0'):
-        self.betas = betas.to(device)
+    def __init__(self, betas: torch.Tensor, noise_model):
+        self.device = betas.device
+        self.betas = betas
         self.alphas = 1 - betas
-        self.alpha_bars = torch.cumprod(self.alphas, dim=0).to(device)
+        self.alpha_bars = torch.cumprod(self.alphas, dim=0).to(self.device)
         self.noise_model = noise_model
-        self.device = device
 
     def sample(self, x, t):
         """
@@ -24,10 +24,10 @@ class DDPMSampler():
 
             Parameters
             ----------
-            x: torch.tensor
+            x: torch.tensor of shape [N, C, D]
                 Sample of previous time step
 
-            t: int
+            t: torch.tensor of shape [N]
                 Time step
                 
             Returns
@@ -38,5 +38,58 @@ class DDPMSampler():
 
         scale = (1 - self.alphas[t+1]) / torch.sqrt(1 - self.alpha_bars[t+1])
         sigma =  (1 - self.alpha_bars[t]) * (1 - self.alphas[t+1]) / (1 - self.alpha_bars[t+1])
-        x = (1 / torch.sqrt(self.alphas[t+1])) * (x - scale * self.noise_model(x, t+1)) + torch.sqrt(sigma) * torch.randn_like(x).to(self.device)
-        return x
+        inv = (1 / torch.sqrt(self.alphas[t+1]))
+
+        scale = scale[:, None, None]
+        sigma = sigma[:, None, None]
+        inv = inv[:, None, None]
+
+        sample = inv * (x - scale * self.noise_model(x, t+1)) + torch.sqrt(sigma) * torch.randn_like(x).to(self.device)
+        return sample
+
+class DDPMSampler2d():
+    """
+        Denoising Diffusion Probabilistic Model (DDPM) sampler for 2D data.
+        Reference: https://arxiv.org/pdf/2006.11239.pdf
+
+        Parameters
+        ----------
+        betas: torch.Tensor of shape [T]
+            Beta values for each time step
+    """
+
+    def __init__(self, betas: torch.Tensor, noise_model):
+        self.device = betas.device
+        self.betas = betas
+        self.alphas = 1 - betas
+        self.alpha_bars = torch.cumprod(self.alphas, dim=0).to(self.device)
+        self.noise_model = noise_model
+
+    def sample(self, x, t):
+        """
+            Sample from the DDPM model for 2D data.
+
+            Parameters
+            ----------
+            x: torch.tensor of shape [N, C, H, W]
+                2D Sample of previous time step
+
+            t: torch.tensor of shape [N]
+                Time step
+                
+            Returns
+            -------
+            samples: torch.Tensor of shape [N, C, H, W]
+                2D Sample at t - 1 from the DDPM model
+        """
+
+        scale = (1 - self.alphas[t+1]) / torch.sqrt(1 - self.alpha_bars[t+1])
+        sigma =  (1 - self.alpha_bars[t]) * (1 - self.alphas[t+1]) / (1 - self.alpha_bars[t+1])
+        inv = (1 / torch.sqrt(self.alphas[t+1]))
+        
+        scale = scale[:, None, None, None]
+        sigma = sigma[:, None, None, None]
+        inv = inv[:, None, None, None]
+
+        sample = inv * (x - scale * self.noise_model(x, t+1)) + torch.sqrt(sigma) * torch.randn_like(x).to(self.device)
+        return sample
